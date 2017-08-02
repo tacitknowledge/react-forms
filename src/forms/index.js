17ceee67c9ee7334c7974c5b06d870114722cecd
Mitu Rostislav <miturostislav@tkmd-221.tacitknowledge.com>
2017-08-02 17:59:24 +0300

    update bundle

--- src/forms/index.js
+++ src/forms/index.js
@@ -4,6 +4,7 @@ import FormGroupAsync from './formGroupAsync';
 import FormError from './formError';
 import FormReducer from './formReducer';
 import * as FormActions from './formActions';
+import * as FormConstants from './formConstants';
 
 const forms = {
   Form,
@@ -11,6 +12,7 @@ const forms = {
   FormGroupAsync,
   FormError,
   FormReducer,
-  FormActions
+  FormActions,
+  FormConstants
 };
 export default forms;
