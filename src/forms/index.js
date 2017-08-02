5b6bdde0b96540617d40ab83a8e23cf72b16a1db
Mitu Rostislav <miturostislav@27.0.168.192.in-addr.arpa>
2017-08-02 22:56:55 +0300

    add validateExpiryYear

--- src/forms/index.js
+++ src/forms/index.js
@@ -5,6 +5,7 @@ import FormError from './formError';
 import FormReducer from './formReducer';
 import * as FormActions from './formActions';
 import * as FormConstants from './formConstants';
+import validateExpiryYear from '../validators/adapters/expiryYear';
 
 const forms={
   Form,
@@ -13,7 +14,8 @@ const forms={
   FormError,
   FormReducer,
   FormActions,
-  FormConstants
+  FormConstants,
+  validateExpiryYear
 };
 
 export default forms;
